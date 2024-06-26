a6588eedb9c3d01a66233dd877d3f4ea3aec5a73
Joaquin Ignacio Fritz Carrizo <d48125437@alumnos.itr.edu.ar>
2024-06-26 11:29:28 -0300

    Ejercicio 1 parte 1

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,11 +1,23 @@
+import './Ejercicio1.css';
+import {useState} from "react";
 export default function Ejercicio1() {
-
+  const [productos, Setproductos] = useState({
+    'Set de cables':'$'+10,
+    'Batería':'$'+50,
+    'Óptica':'$'+100
+  });
   return (
     <div>
       <h2>Ejercicio 1: Lista desplegable</h2>
 
       <label>Seleccione un producto: </label>
-      {/* Crear lista de selección */}
+      <select className="select-box">
+      {Object.keys(productos).map((producto, index) => (
+          <option key={index} value={producto}>
+            {producto} (Precio: {productos[producto]})
+          </option>
+        ))}
+      </select>
 
     </div>
   )
